11557ed56e03ab0834b84ac7522c8c1146f0e14b
Vojtěch Perník <develop@pervoj.cz>
2024-11-23 19:19:21 +0100

    feat: add bluetooth state component

---
+++ mobile/src/components/bluetooth-state.tsx
@@ -0,0 +1,52 @@
+import { BluetoothOff, BluetoothSearching } from "lucide-react-native";
+import { ReactNode } from "react";
+import { Text } from "react-native";
+import { State } from "react-native-ble-plx";
+import { Banner, BannerContent, BannerHeader } from "./banner";
+import { useBle } from "./ble-context";
+
+export default function BluetoothState({ children }: { children?: ReactNode }) {
+  const { state } = useBle();
+
+  if (!state || state == State.Unknown || state == State.Resetting) {
+    return (
+      <Banner variant="info">
+        <BannerHeader icon={BluetoothSearching} title="Načítání stavu" />
+        <BannerContent>
+          <Text>
+            Aplikace se snaží zjistit stav Bluetooth a připojení k zařízení.
+          </Text>
+        </BannerContent>
+      </Banner>
+    );
+  }
+
+  if (state == State.Unauthorized || state == State.Unsupported) {
+    return (
+      <Banner variant="error">
+        <BannerHeader icon={BluetoothOff} title="Přístup k Bluetooth" />
+        <BannerContent>
+          <Text>
+            Aplikace nemá povolení k používání Bluetooth nebo zařízení
+            nepodporuje Bluetooth.
+          </Text>
+        </BannerContent>
+      </Banner>
+    );
+  }
+
+  if (state == State.PoweredOff) {
+    return (
+      <Banner variant="warning">
+        <BannerHeader icon={BluetoothOff} title="Vypnutý Bluetooth" />
+        <BannerContent>
+          <Text>
+            Bluetooth je vypnutý. Pro pokračování je nutné jej zapnout.
+          </Text>
+        </BannerContent>
+      </Banner>
+    );
+  }
+
+  return children;
+}
